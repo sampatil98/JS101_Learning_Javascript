4dcff054ebf72df6e30504b9fe99b3745da7e7a4
sambhaji dhore <sambhajisd4@gmail.com>
2022-10-12 19:18:21 +0530

    assignment added

---
+++ Array/problem1.js
@@ -0,0 +1,5 @@
+// Given an array print the position (starting with 1) and the element in a single line.
+let arr = ["sambhaji","omkar","namdev","dnyaneshwar"];
+for(let i=0;i<=arr.length-1;i++){
+  console.log(i+1,arr[i]);
+}
